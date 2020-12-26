0f10a0661b85a028bca6bd968ad4a49432e079ae
Thomas Eddy <me@it3d.co>
2020-12-26 21:39:09 +0000

    Create daily playlists

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"time"
 
 	"github.com/imtomeddy/bbc-radio-spotify/internal/constants"
 	"github.com/imtomeddy/bbc-radio-spotify/internal/jobs"
@@ -13,6 +15,13 @@ func main() {
 
 	log.Println("Starting BBC Radio to Spotify Service")
 	log.Printf("Supported Radio Stations: (%d) %s", len(constants.Stations), constants.Stations)
+
+	for _, station := range constants.Stations {
+		playlistName := fmt.Sprintf("%s | All Tracks Daily | %s", constants.StationNames[station], time.Now().Format("02/01/2006"))
+		playlistDesc := "Playlist generated using bbc-radio-spotify (https://github.com/imtomeddy/bbc-radio-spotify)."
+
+		spotifyclient.GetPlaylist(station+"_daily", playlistName, playlistDesc)
+	}
 
 	jobs.SetupJobs()
 
