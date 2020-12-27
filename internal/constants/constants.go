d24d32ee1d066d1fb4961a23dfca1687a1721d2e
Thomas Eddy <me@it3d.co>
2020-12-27 11:09:09 +0000

    Reduce amount of supported stations
    After 12 hours there are already 80+ playlists. This will hit the Spotify limit fast, so reducing to 4 stations will help.

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -17,10 +17,6 @@ var Stations = [...]string{
 	"bbc_radio_one_dance",
 	"bbc_1xtra",
 	"bbc_radio_two",
-	"bbc_radio_three",
-	"bbc_6music",
-	"bbc_asian_network",
-	"bbc_radio_wales_fm",
 }
 
 //StationNames are the nice names for stations
@@ -29,8 +25,4 @@ var StationNames map[string]string = map[string]string{
 	"bbc_radio_one_dance": "BBC Radio 1 Dance",
 	"bbc_1xtra":           "BBC Radio 1Xtra",
 	"bbc_radio_two":       "BBC Radio Two",
-	"bbc_radio_three":     "BBC Radio Three",
-	"bbc_6music":          "BBC 6 Music",
-	"bbc_asian_network":   "BBC Asian Network",
-	"bbc_radio_wales_fm":  "BBC Radio Wales",
 }
